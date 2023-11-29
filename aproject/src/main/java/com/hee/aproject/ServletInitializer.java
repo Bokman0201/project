8eb5d7ebe7091d62a95e15ecbbe7ca3958c35142
Bokman0201 <jeonghs24@nate.com>
2023-11-29 14:07:04 +0900

    create

---
+++ aproject/src/main/java/com/hee/aproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.hee.aproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AprojectApplication.class);
+	}
+
+}
---
+++ aproject/src/main/java/com/hee/aproject/dto/ProjectDto.java
@@ -0,0 +1,13 @@
+package com.hee.aproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class ProjectDto {
+	
+	private String name;
+
+}
